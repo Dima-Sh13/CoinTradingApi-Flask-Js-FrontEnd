f67048526ad0995a7700880faadcc9983c983e58
Dima Shalupnya <dshalupnya@gmail.com>
2025-10-21 12:36:54 +0200

    conexion a api

--- my_coin/conection.py
+++ my_coin/conection.py
@@ -1,3 +1,18 @@
 import sqlite3
-import requests
-from config import DATA_BASE
+from requests import Request, Session
+
+import json
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+def conexion():
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': "e197777064bf438bbf96a6c2145bb120",
+    }
+
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(url)
+    data = json.loads(response.text)
+    return data
--- my_coin/routes.py
+++ my_coin/routes.py
@@ -1,8 +1,19 @@
 from my_coin import app
-from flask import redirect, render_template, requests, jsonify
-
+from flask import redirect, render_template, request, jsonify
+from my_coin.conection import *
+import requests
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+"""
+@app.route("/api/v1/endpoint1")
+def prueba():
+    datos = conexion()    
+    return jsonify ({
+
+        "datos": datos
+        "status": "ok" 
+    })
+""" 
